5601fb14959ae3fb58d81b3d6d9f98b8fe0049e8
Ghislain Leblanc <ghisleb@me.com>
2025-03-14 11:13:31 -0400

    Empty project

---
+++ ARTM/ARTM/ARTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTMApp.swift
+//  ARTM
+//
+//  Created by Ghislain Leblanc on 2025-03-14.
+//
+
+import SwiftUI
+
+@main
+struct ARTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARTM/ARTMTests/ARTMTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARTMTests.swift
+//  ARTMTests
+//
+//  Created by Ghislain Leblanc on 2025-03-14.
+//
+
+import Testing
+@testable import ARTM
+
+struct ARTMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
